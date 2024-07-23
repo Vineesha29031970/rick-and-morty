653ad834f6385f55470608b7a343af64b73e8ac8
Vineesha29031970 <22082k3@gmail.com>
2024-07-23 14:27:34 +0530

    final commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import CartoonComponents from './Components/Cartoon Components/CartoonComponents'
+
+
+function App() {
+  return (
+    <CartoonComponents/>
+  )
+}
+
+export default App
